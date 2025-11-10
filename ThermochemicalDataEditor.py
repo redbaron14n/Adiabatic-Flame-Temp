03ab03fcc8f19ded9e8d406ab5331d53c72f235b
Ian <redbaron14n@gmail.com>
2025-11-10 12:29:57 -0600

    Created compound list and added representation method to compound class.

--- ThermochemicalDataEditor.py
+++ ThermochemicalDataEditor.py
@@ -14,22 +14,28 @@ TEMPERATURE_LIST = np.array([0, 100, 200, 250, 298.15, 300, 350, 400, 450, 500, 
 
 class Compound:
 
-    def __init__(self, formula: str, sensible_heat: NDArray[np.float64], heat_of_formation: NDArray[np.float64], logKf: NDArray[np.float64], temperature: NDArray[np.float64] = TEMPERATURE_LIST):
+    ##### Initialization #####
+
+    def __init__(self, name: str, formula: str, sensible_heat: NDArray[np.float64], heat_of_formation: NDArray[np.float64], logKf: NDArray[np.float64], temperature: NDArray[np.float64] = TEMPERATURE_LIST):
 
         self.__set_formula(formula)
+        self.__set_name(name)
         self.__set_temp_list(temperature)
         self.__set_sh_list(sensible_heat)
         self.__set_hf_list(heat_of_formation)
         self.__set_logKf_list(logKf)
 
+    def __repr__(self):
+
+        return f"{self.name} ({self.formula})"
+
     def __set_formula(self, formula: str):
 
-        self.__formula = formula
+        self.formula = formula
+    
+    def __set_name(self, name: str):
 
-    def formula(self) -> str:
-
-        formula = self.__formula
-        return formula
+        self.name = name
 
     def __set_temp_list(self,t: NDArray[np.float64]):
 
@@ -92,6 +98,8 @@ class Compound:
 
         logKf = self.__logKf_list
         return logKf
+    
+    ##### Methods #####
 
     def logKf(self, temperature: float) -> float:
 
@@ -118,6 +126,8 @@ class Compound:
         if transpose:
             table = np.transpose(table)
         return table
+    
+Compound_list = []
 
 ##### Methane (CH4) #####
 
@@ -125,4 +135,5 @@ methane_sh = np.array([-10.024, -6.698, -3.368, -1.679, 0, 0.066, 1.903, 3.861, 
 methane_hf = np.array([-66.911, -69.644, -72.027, -73.426, -74.873, -74.929, -76.461, -77.969, -79.422, -80.802, -83.308, -85.452, -87.238, -88.692, -89.849, -90.750, -91.437, -91.945, -92.308, -92.553, -92.703, -92.780, -92.797, -92.770, -92.709, -92.624, -92.521, -92.409, -92.291, -92.174, -92.060, -91.954, -91.857, -91.773, -91.705, -91.653, -91.621, -91.609, -91.619, -91.654, -91.713, -91.798, -91.911, -92.051, -92.222, -92.422, -92.652, -92.914, -93.208, -93.533, -93.891, -94.281, -94.702, -95.156, -95.641, -96.157, -96.703, -97.278, -97.882, -98.513, -99.170, -99.852, -100.557, -101.284, -102.032])
 methane_logkf = np.array([np.inf, 33.615, 15.190, 11.395, 8.894, 8.813, 6.932, 5.492, 4.350, 3.420, 1.993, 0.943, 0.138, -0.500, -1.018, -1.447, -1.807, -2.115, -2.379, -2.609, -2.810, -2.989, -3.147, -3.289, -3.416, -3.531, -3.636, -3.732, -3.819, -3.899, -3.973, -4.042, -4.105, -4.164, -4.219, -4.271, -4.319, -4.364, -4.407, -4.447, -4.485, -4.521, -4.555, -4.588, -4.619, -4.648, -4.676, -4.703, -4.729, -4.753, -4.777, -4.800, -4.822, -4.843, -4.863, -4.883, -4.902, -4.920, -4.938, -4.955, -4.972, -4.988, -5.004, -5.019, -5.034])
 
-methane = Compound("CH4", methane_sh, methane_hf, methane_logkf)
+Methane = Compound("Methane", "CH4", methane_sh, methane_hf, methane_logkf)
+Compound_list.append(Methane)
