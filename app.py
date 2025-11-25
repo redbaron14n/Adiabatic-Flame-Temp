c700ba05436e1d5a1794f776cf30df2e6008ff65
Ian <redbaron14n@gmail.com>
2025-11-24 18:24:13 -0600

    Added UI for reaction or compound modes in app.py.

--- app.py
+++ app.py
@@ -38,5 +38,69 @@ app.layout = html.Div([
     dcc.Graph(id = "main-graph")
 ])
 
+@app.callback(
+    Output("mode-container", "children"),
+    Input("mode-dropdown", "value")
+)
+
+def display_mode(mode):
+
+    if mode == "compound":
+        return compound_controls()
+    elif mode == "reaction":
+        return reaction_controls()
+    
+def compound_controls():
+
+    return html.Div([
+        html.Label("Compound"),
+        dcc.Dropdown(
+            id="compound-dropdown",
+            options = [{"label": c.name, "value": c.name} for c in Compound_list],
+            value = Compound_list[0].name
+        ),
+
+        html.Label("Variable"),
+        dcc.Dropdown(
+            id = "variable-dropdown",
+            options = [
+                {"label": "Constant Pressure Specific Heat (Cp)", "value": "cp"},
+                {"label": "Standard Entropy (S°)", "value": "s"},
+                {"label": "Total Entropy Change (ΔS)", "value": "ds"},
+                {"label": "Sensible Heat (SH)", "value": "sh"},
+                {"label": "Heat of Formation (ΔHf)", "value": "hf"},
+                {"label": "Gibbs Free Energy (ΔGf)", "value": "gf"},
+                {"label": "log Kf", "value": "logKf"}
+            ],
+            value = "sh"
+        )
+    ])
+
+def reaction_controls():
+
+    return html.Div([
+        html.Label("Number of Reactants"),
+        dcc.Dropdown(
+            id = "num-reactants",
+            options = [{"label": str(i), "value": i} for i in range(1, MAX_REACTANTS+1)],
+            value = 2
+        ),
+
+        html.Div(id = "reactant-selectors"),
+
+        html.Label("Reactant with Variable Concentration"),
+        dcc.Dropdown(
+            id = "variable-reactant"
+        ),
+
+        html.Div(id="fixed-reactant-inputs"),
+
+        dcc.Checklist(
+            id = "dissociation-check",
+            options = [{"label": "Enable dissociation", "value": "diss"}],
+            value = []
+        )
+    ])
+
 if __name__ == "__main__":
     app.run(debug=True)
