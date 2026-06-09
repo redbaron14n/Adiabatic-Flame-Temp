9afc3c7609398e8b2ee7fa5b6d7363324b58b607
redbaron14n <redbaron14n@gmail.com>
2026-06-09 13:21:57 -0500

    Adding test file to repository

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+from domain.compounds import compounds
 from domain.dissociativeflametemp import DissociativeReaction
 from numpy import array
 
 
-guess = array([750.0, 0.5, 0.5, 0.1, 0.4, 0.7, 0.2, 0.9, 0.1, 0.5, 0.1])
+guess = array([3000.0, 0.5, 0.5, 0.1, 0.4, 0.7, 0.2, 0.9, 0.1, 0.5, 0.1])
 species_indices = {"T": 0, "Methane": 1, "Oxygen": 2, "Carbon_Dioxide": 3, "Water": 4, "Carbon_Monoxide": 5, "Oxygen_Monatomic": 6, "Carbon": 7, "Hydrogen": 8, "Hydroxyl": 9, "Hydrogen_Monatomic": 10}
+
+guess_actual = array([0.9, 0.1, 0.7, 0.1, 0.1, 0.5, 0.5, 0.5, 0.2, 0.4, 3000.0])
 
 
 test_reaction = DissociativeReaction(
@@ -14,4 +17,17 @@ test_reaction = DissociativeReaction(
         conc_res = 5
     )
 
-print(test_reaction._equilibrium_residual("Methane", guess, species_indices)) # This does not agree with manual calculation.
+# print(test_reaction._equilibrium_residual("Methane", guess, species_indices)) # This does not agree with manual calculation.)
+
+print(test_reaction.equilibrate(1))
+
+# methane = compounds["Methane"]
+# carbon_monoxide = compounds["Carbon_Monoxide"]
+# oxygen = compounds["Oxygen"]
+# hydrogen_monatomic = compounds["Hydrogen_Monatomic"]
+# for species in [methane, carbon_monoxide, oxygen, hydrogen_monatomic]:
+#     for temp in [300, 1000, 2000, 3000]:
+#         print(f"{species.name} logKf at {temp} K: {species.logKf(temp)}")
+
+for diss_obj in test_reaction._dissociations.values():
+    print(f"{diss_obj.molecule_id} dissociates into {diss_obj._stoich}")
