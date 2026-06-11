5a9d15386a4fa6bf47d2119db042440038e856d7
redbaron14n <redbaron14n@gmail.com>
2026-06-11 15:43:28 -0500

    Added conc_list setter to new flame temp file

--- domain/adiabatic_flame_temp.py
+++ domain/adiabatic_flame_temp.py
@@ -4,22 +4,35 @@
 # Adiabatic Flame Temperature Calculator
 # ###################
 
-from domain.compound import Compound
+from domain.compounds import compounds
 
 class CombustionReaction:
 
     def __init__(
             self,
-            fuels: dict[Compound, float],
-            oxidants: dict[Compound, float],
-            pressure: float,
-            temps: dict[Compound, float]
+            fuels: dict[str, float],
+            oxidants: dict[str, float],
+            temps: dict[str, float],
+            pressure: float = 1e5,
+            conc_res: int = 100
         ):
+
+        """
+        Initializes a new instance of the CombustionReaction class.
+
+        :param dict[str, float] fuels: A dictionary mapping fuel compounds to their respective amounts.
+        :param dict[str, float] oxidants: A dictionary mapping oxidant compounds to their respective amounts.
+        :param dict[str, float] temps: A dictionary mapping compounds to their respective temperatures [K].
+        :param float pressure: The pressure of the combustion reaction [Pa].
+        :param int conc_res: The concentration resolution for the calculation.
+        """
         
         self._set_fuels(fuels)
         self._set_oxidants(oxidants)
         self.pressure = pressure
         self.temperatures = temps
+        self.concentration_resolution = conc_res
+        self._set_conc_list()
 
 
     ########################################
@@ -27,17 +40,21 @@ class CombustionReaction:
     ########################################
 
 
-    def _set_fuels(self, fuels: dict[Compound, float]):
+    def _set_fuels(self, fuels: dict[str, float]):
 
-        for amount in fuels.values():
+        for fuel, amount in fuels.items():
+            if fuel not in compounds:
+                raise ValueError("Invalid fuel compound.")
             if amount <= 0:
                 raise ValueError("Fuel amounts must be positive values.")
         self._fuels = self._normalize_ratio(fuels)
 
 
-    def _set_oxidants(self, oxidants: dict[Compound, float]):
+    def _set_oxidants(self, oxidants: dict[str, float]):
 
-        for amount in oxidants.values():
+        for oxidant, amount in oxidants.items():
+            if oxidant not in compounds:
+                raise ValueError("Invalid oxidant compound.")
             if amount <= 0:
                 raise ValueError("Oxidant amounts must be positive values.")
         self._oxidants = self._normalize_ratio(oxidants)
@@ -64,7 +81,7 @@ class CombustionReaction:
     
 
     @temperatures.setter
-    def temperatures(self, value: dict[Compound, float]):
+    def temperatures(self, value: dict[str, float]):
 
         for temp in value.values():
             if temp < 0:
@@ -72,12 +89,37 @@ class CombustionReaction:
         self._temperatures = value
 
 
+    @property
+    def concentration_resolution(self):
+
+        return self._conc_res
+    
+
+    @concentration_resolution.setter
+    def concentration_resolution(self, value: int):
+
+        if value < 2:
+            raise ValueError("Concentration resolution must be at least 2.")
+        self._conc_res = value
+
+
+    def _set_conc_list(self):
+
+        denom = self._conc_res + 1 # Allows for desired resolution without 0 and 1 ratios (no flame)
+        conc_list: list[dict[str, float]] = []
+        for i in range(1, denom):
+            fuel_ratio = i / denom
+            conc_list.append({fuel: fuel_mix*fuel_ratio for fuel, fuel_mix in self._fuels.items()})
+            conc_list[i-1].update({oxidant: oxi_mix*(1-fuel_ratio) for oxidant, oxi_mix in self._oxidants.items()})
+        self._conc_list = conc_list
+
+
     ########################################
     # Private Methods
     ########################################
 
 
-    def _normalize_ratio(self, ratio: dict[Compound, float]) -> dict[Compound, float]:
+    def _normalize_ratio(self, ratio: dict[str, float]) -> dict[str, float]:
 
         total = sum(ratio.values())
         return {compound: amount / total for compound, amount in ratio.items()}
