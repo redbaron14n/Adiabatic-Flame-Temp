5a7674e8d8dbfde05945f71b116121e31a2fec43
redbaron14n <redbaron14n@gmail.com>
2026-06-15 16:56:32 -0500

    Just trying again. Need to go back to log form but not there yet

--- domain/adiabatic_flame_temp.py
+++ domain/adiabatic_flame_temp.py
@@ -8,6 +8,7 @@ from config import determine_basic_products
 from domain.compounds import compounds
 from domain.dissociation import Dissociation
 from numpy.typing import NDArray
+from scipy.optimize import fsolve, least_squares, OptimizeResult
 import numpy as np
 
 NPS_EXP_FACTOR: float = 3000 # The factor by which to multiply the exponent of the residual of the non-physical solution. Enforces mass balance. Arbitrary number
@@ -44,6 +45,7 @@ class CombustionReaction:
         self._set_item_indices()
         self._set_residual_indices()
         self._set_dissociations()
+        # self._set_bounds()
 
 
     ########################################
@@ -186,14 +188,24 @@ class CombustionReaction:
             if compounds[compound].composition: # If not elemental
                 products.append(compound)
         self._residual_indices = {product: idx for idx, product in enumerate(products)}
+        self._residual_indices["T"] = len(self._residual_indices)
 
 
     def _set_dissociations(self):
 
         self._dissociations: dict[str, Dissociation] = {}
         for compound in self._residual_indices.keys():
+            if compound == "T":
+                continue
             components = set(compounds[compound].composition.keys())
             self._dissociations[compound] = Dissociation(compound, components)
+
+
+    # def _set_bounds(self):
+
+    #     guess_length = len(self._independents)
+    #     self._lower_bounds: NDArray[np.float64] = np.full(guess_length + 1, 0.0)
+    #     self._upper_bounds: NDArray[np.float64] = np.concatenate((np.full(guess_length, 5.0), np.array([6000.0])))
 
 
     ########################################
@@ -228,45 +240,48 @@ class CombustionReaction:
         For a given guess of the independent species log quantities and temperature, calculates the full intended guess vector, including dependent species quantities.
 
         :param NDArray[np.float64] init_atoms: The initial number of each type of atom in the mixture.
-        :param NDArray[np.float64] input_guess: The current guess of the independent species quantities and temperature, in log form for the species quantities.
+        :param NDArray[np.float64] input_guess: The current guess of the independent species log quantities and temperature.
         :return NDArray[np.float64]: The calculated guess vector including dependent species quantities.
         :return bool: Whether the calculated dependent quantities are physically valid (non-negative).
         """
 
-        indep_qty_vector = np.zeros(len(self._independents))
-        for i, indep in enumerate(self._independents):
-            indep_qty_vector[i] = 10**input_guess[i] # Convert log quantities to actual quantities.
+        temp_index = self._residual_indices["T"]
+        indep_qty_vector: NDArray[np.float64] = 10**np.delete(input_guess, temp_index)
         dep_qty_vector = self._inv_dep_matr @ (init_atoms - (self._indep_matr @ indep_qty_vector))
-        temp_index = self._item_indices["T"]
-        true_guess = np.concatenate([indep_qty_vector, [input_guess[temp_index]], dep_qty_vector])
-        is_valid = bool(np.all(true_guess >= 0))
-        return true_guess, is_valid
+        full_guess = np.concatenate([indep_qty_vector, [input_guess[temp_index]], dep_qty_vector])
+        is_valid = bool(np.all(full_guess >= 0))
+        return full_guess, is_valid
     
 
-    def _calc_residuals(self, guess: NDArray[np.float64]) -> NDArray[np.float64]:
+    def _calc_equil_residual(self, compound: str, full_guess: NDArray[np.float64], is_valid: bool) -> float:
 
-        residuals = np.full(len(self._independents), 1000, dtype=np.float64)
-        for diss_reaction in self._dissociations.values():
-            resid = diss_reaction.equilibrium_residual(guess, self._item_indices, self._pressure)
-            indx = self._residual_indices[diss_reaction.molecule_id]
-            residuals[indx] = resid
+        if not is_valid:
+            penalty = np.min(full_guess) * 1e4 # Need to go back to log form to make this actually potent
+            return penalty
+        residual = self._dissociations[compound].equilibrium_residual(full_guess, self._item_indices, self._pressure)
+        return residual
+    
+
+    def _residual_function(self, guess, conc_dict):
+
+        init_atoms = self._calc_init_atoms(conc_dict)
+        full_guess, is_valid = self._calc_guess_vector(init_atoms, guess)
+        residuals: NDArray[np.float64] = np.zeros(len(self._residual_indices))
+        for compound in self._independents:
+            residual = self._calc_equil_residual(compound, full_guess, is_valid)
+            residuals[self._residual_indices[compound]] = residual
+        residuals[self._residual_indices["T"]] = self._calc_heat_residual(full_guess, is_valid)
         return residuals
     
 
-    def _residual_function(self, conc_idx: int, guess: NDArray[np.float64]) -> NDArray[np.float64]:
+    ########################################
+    # Public Methods
+    ########################################
 
-        """
-        Process input arguments and calculates the residuals for a given concentration index and guess vector.
-        """
 
-        conc_dict = self._conc_list[conc_idx]
-        print(conc_dict)
-        init_atoms = self._calc_init_atoms(conc_dict)
-        true_guess, is_valid = self._calc_guess_vector(init_atoms, guess)
-        print(true_guess)
-        residual = self._calc_residuals(true_guess)
-        print(residual)
-        # if not is_valid: # Blow up a non-physical solution
-        #     min_mag = -np.min(true_guess) # Always positive
-        #     residual *= 10**(NPS_EXP_FACTOR * min_mag)
-        return residual
+    def calculate_temperatures(self):
+
+        temps: list[float] = []
+        for conc_dict in self._conc_list:
+            init_guess = self._calc_init_guess(conc_dict)
+            equil = least_squares(self._residual_function, init_guess, args=(conc_dict,), bounds=self._bounds)
