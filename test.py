7069a8f0d59381d67e94c7d61d3087729fca98fb
redbaron14n <redbaron14n@gmail.com>
2026-06-15 16:56:44 -0500

    Including test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from domain.compounds import compounds
+from domain.dissociation import Dissociation
 from domain.dissociativeflametemp import DissociativeReaction
 from domain.adiabatic_flame_temp import CombustionReaction
 from math import log10
@@ -9,16 +10,10 @@ test_reaction = CombustionReaction(
     fuels = {"Methane": 1},
     oxidants = {"Oxygen": 1},
     temps = {"Methane": 298.15, "Oxygen": 298.15},
-    pressure = 1e5
+    pressure = 1e5,
+    conc_res = 99
 )
 
 init_atoms = np.array([1.32, 0.33, 1.34])
-init_guess = np.full(8, -0.3)
-init_guess[7] = 3000.0
-
+init_guess = np.array([-0.48149, -1, -1, -1, -1, -1, -0.17393, 3000.0])
 print(test_reaction._calc_guess_vector(init_atoms, init_guess))
-print(test_reaction._item_indices)
-print(test_reaction._residual_indices)
-print(test_reaction._residual_function(32, init_guess))
-
-# print(np.any(init_guess <= 0))
