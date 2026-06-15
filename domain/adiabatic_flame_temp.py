d9c0cb0586b72df5ddb26b1f5ca8badc14e30c88
redbaron14n <redbaron14n@gmail.com>
2026-06-15 11:27:29 -0500

    Started work on residual architecture for the new class. Need to figure out best way to bridge between regular and log quantities

--- domain/adiabatic_flame_temp.py
+++ domain/adiabatic_flame_temp.py
@@ -6,8 +6,11 @@
 
 from config import determine_basic_products
 from domain.compounds import compounds
+from domain.dissociation import Dissociation
 from numpy.typing import NDArray
 import numpy as np
+
+NPS_EXP_FACTOR: float = 3000 # The factor by which to multiply the exponent of the residual of the non-physical solution. Enforces mass balance. Arbitrary number
 
 class CombustionReaction:
 
@@ -39,6 +42,8 @@ class CombustionReaction:
         self._set_dependents()
         self._set_independents()
         self._set_item_indices()
+        self._set_residual_indices()
+        self._set_dissociations()
 
 
     ########################################
@@ -124,10 +129,10 @@ class CombustionReaction:
 
         if "Methane" in self._fuels:
             self._dependents = ["Methane", "Oxygen", "Water"]
-            self._dep_matr = np.linalg.inv(np.array([[4, 0, 2], [1, 0, 0], [0, 2, 1]]))
+            self._inv_dep_matr = np.linalg.inv(np.array([[4, 0, 2], [1, 0, 0], [0, 2, 1]]))
         elif "Hydrogen" in self._fuels:
             self._dependents = ["Hydrogen", "Oxygen", "Water"]
-            self._dep_matr = np.linalg.inv(np.array([[2, 0, 2], [0, 0, 0], [0, 2, 1]]))
+            self._inv_dep_matr = np.linalg.inv(np.array([[2, 0, 2], [0, 0, 0], [0, 2, 1]]))
         else:
             raise ValueError("Invalid fuel compound.")
         
@@ -173,6 +178,24 @@ class CombustionReaction:
         self._item_indices.update({dep: idx + offset for idx, dep in enumerate(self._dependents)})
 
 
+    def _set_residual_indices(self):
+
+        all_species = self._independents + self._dependents
+        products: list[str] = []
+        for compound in all_species:
+            if compounds[compound].composition: # If not elemental
+                products.append(compound)
+        self._residual_indices = {product: idx for idx, product in enumerate(products)}
+
+
+    def _set_dissociations(self):
+
+        self._dissociations: dict[str, Dissociation] = {}
+        for compound in self._residual_indices.keys():
+            components = set(compounds[compound].composition.keys())
+            self._dissociations[compound] = Dissociation(compound, components)
+
+
     ########################################
     # Private Methods
     ########################################
@@ -199,18 +222,51 @@ class CombustionReaction:
         return init_atoms
     
 
-    def _calc_dependent_qty(self, init_atoms: NDArray[np.float64], input_guess: NDArray[np.float64]) -> NDArray[np.float64], bool:
+    def _calc_guess_vector(self, init_atoms: NDArray[np.float64], input_guess: NDArray[np.float64]) -> tuple[NDArray[np.float64], bool]:
 
         """
-        For a given guess of the independent species quantities and temperature, calculates the quantities of the dependent species.
+        For a given guess of the independent species log quantities and temperature, calculates the full intended guess vector, including dependent species quantities.
 
         :param NDArray[np.float64] init_atoms: The initial number of each type of atom in the mixture.
         :param NDArray[np.float64] input_guess: The current guess of the independent species quantities and temperature, in log form for the species quantities.
-        :return NDArray[np.float64]: The calculated quantities of the dependent species.
+        :return NDArray[np.float64]: The calculated guess vector including dependent species quantities.
         :return bool: Whether the calculated dependent quantities are physically valid (non-negative).
         """
 
         indep_qty_vector = np.zeros(len(self._independents))
         for i, indep in enumerate(self._independents):
             indep_qty_vector[i] = 10**input_guess[i] # Convert log quantities to actual quantities.
-        # Not done
+        dep_qty_vector = self._inv_dep_matr @ (init_atoms - (self._indep_matr @ indep_qty_vector))
+        is_valid = bool(np.all(dep_qty_vector >= 0))
+        temp_index = self._item_indices["T"]
+        true_guess = np.concatenate([indep_qty_vector, [input_guess[temp_index]], dep_qty_vector])
+        return true_guess, is_valid
+    
+
+    def _calc_residuals(self, guess: NDArray[np.float64]) -> NDArray[np.float64]:
+
+        residuals = np.full(len(self._independents), 1000, dtype=np.float64)
+        for diss_reaction in self._dissociations.values():
+            resid = diss_reaction.equilibrium_residual(guess.tolist(), self._item_indices, self._pressure) # This currently uses logarithmic quantities. WRONG INPUT TYPE
+            indx = self._residual_indices[diss_reaction.molecule_id]
+            residuals[indx] = resid
+        return residuals
+    
+
+    def _residual_function(self, conc_idx: int, guess: NDArray[np.float64]) -> NDArray[np.float64]:
+
+        """
+        Process input arguments and calculates the residuals for a given concentration index and log guess vector.
+        """
+
+        conc_dict = self._conc_list[conc_idx]
+        print(conc_dict)
+        init_atoms = self._calc_init_atoms(conc_dict)
+        true_guess, is_valid = self._calc_guess_vector(init_atoms, guess)
+        print(true_guess)
+        residual = self._calc_residuals(true_guess)
+        print(residual)
+        if not is_valid: # Blow up a non-physical solution
+            min_mag = -np.min(true_guess) # Always positive
+            residual *= 10**(NPS_EXP_FACTOR * min_mag)
+        return residual
